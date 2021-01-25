dc0ab9656b2c4dd125a9258d9e42f63fb8610dc7
Xiaoyuer-6 <74359600+Xiaoyuer-6@users.noreply.github.com>
2021-01-25 22:15:59 +0800

    Update MyStack.java

--- MyStack.java
+++ MyStack.java
@@ -1,14 +1,4 @@
-/**
- * Created with IntelliJ IDEA.
- * Description:
- *
- * 类加载器：加载类的时候 ，不同的类加载器加载对应得类。
- *       双亲委派模型。
- *       3个
- * User: GaoBo
- * Date: 2021-01-22
- * Time: 12:05
- */
+
 public class MyStack {
     private int[] elem;
     private int top;//既可以代表下标：这个位置就是当前可以存放数据的下标
